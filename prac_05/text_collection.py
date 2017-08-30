fea2fdf2d5254d87ceb0742a9c39f113f7858e85
jc194412 <adam.davey1@my.jcu.edu.au>
2017-08-30 15:52:37 +1000

    add states add colors add do it from scratch (still to complete)

---
+++ prac_05/text_collection.py
@@ -0,0 +1,2 @@
+TEXT_WORDS = {"a": ,"collection": ,"fun": , "is": , "it": ,"nice": ,"of": , "thing": , "this": , "words":}
+text = this is a collection of worlds of nice words this is a fun thing it is
