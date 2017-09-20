2010db9e4d988733a65a49a8e5c2d194c9a759bc
jc194412 <adam.davey1@my.jcu.edu.au>
2017-09-20 14:39:23 +1000

    add functions to distance conversion

--- prac_06/KivyDemos-master/distance_convert.py
+++ prac_06/KivyDemos-master/distance_convert.py
@@ -1,4 +1,3 @@
-
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
@@ -7,19 +6,32 @@ __author__ = 'Adam Davey'
 
 Miles_to_Kms = 1.60934
 
-class MilesconverterApp(App):
 
+class MilesconverterApp(App):
     def build(self):
-        """ build the Kivy app from the kv file """
-        Window.size = (200, 100)
+
+        Window.size = (400, 200)
         self.title = "miles to kms converter"
-        self.root = Builder.load_file('convert_m_km.kv')
+        self.root = Builder.load_file('distance_convert.kv')
         return self.root
 
-    def handle_calculate(self, value):
-        """ handle calculation (could be button press or other call), output result to label widget """
-        result = value ** 2
-        self.root.ids.output_label.text = str(result)
+    def handle_calculate(self, miles_text):
+
+        try:
+            kms = float(miles_text) * Miles_to_Kms
+            self.root.ids.output_label.text = str("{:.2f}".format(kms))
+        except:
+            ValueError
+
+    def get_validated_miles(self):
+        kms = self.root.ids.input_label.text
+        return
+
+
+        # def handle_increment(self change):
+        #     value = self.get_validated_miles() + change
+        #     self.root.ids.input_miles.text = str(value)
+        #     self.handle_calculate()
 
 
 MilesconverterApp().run()
