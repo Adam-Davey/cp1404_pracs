956a168c168bdb6156efae45d8ed7a5a09ebf8b0
jc194412 <adam.davey1@my.jcu.edu.au>
2017-09-27 13:05:00 +1000

    add languages

---
+++ prac_07/languages.py
@@ -0,0 +1,4 @@
+
+ruby = ProgrammingLanguage("Ruby", "dynamic", True, 1995)
+python = ProgrammingLanguage("Python", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visul Basic", "Static", False, 1991)
---
+++ prac_07/programming_language.py
@@ -0,0 +1,10 @@
+
+class ProgrammingLanguage:
+    def __init__(self):
+
+
+    def is_dynamic(self):
+        if dynamic
+            print("dynamic")
+        else:
+            print ("static")
